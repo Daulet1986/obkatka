e82d578da6f10a7ac6c17850ec8ae9f44f48967c
Aliye <221288@astanait.edu.kz>
2023-02-17 12:38:10 +0600

    search function

--- src/obkatka/DbFunctions.java
+++ src/obkatka/DbFunctions.java
@@ -1,5 +1,4 @@
 package obkatka;
-
 import java.sql.*;
 
 public class DbFunctions {
@@ -22,7 +21,9 @@ public class DbFunctions {
     public void createTable(Connection conn,String table_name){
         Statement statement;
         try {
-            String query="create table "+table_name+"(id serial primary key, IIN varchar(20), name varchar(20), surname varchar(20), age varchar(200), address varchar(20), insurance varchar(20), payment varchar(20));";
+            String query="create table "+table_name+"(id serial primary key, IIN float," +
+                    " name varchar(20), surname varchar(20), gender boolean, age int, address varchar(20)," +
+                    " insurance boolean, payment varchar(20));";
             statement=conn.createStatement();
             statement.executeUpdate(query);
             System.out.println("Table created");
@@ -30,27 +31,36 @@ public class DbFunctions {
             System.out.println(e);
         }
     }
-    public void search_by_UIN(Connection conn,String table_name,int IIN) {
+
+    public void createTableDoc(Connection conn,String table_name){
         Statement statement;
-        ResultSet rs = null;
         try {
-            String query = String.format("select *  from %s where empid= %s", table_name, IIN);
-            statement = conn.createStatement();
-            rs = statement.executeQuery(query);
+            String query="create table "+table_name+"(id serial primary key, name varchar(20), " +
+                    "surname varchar(20), specialty varchar(20), time varchar(50));";
+            statement=conn.createStatement();
+            statement.executeUpdate(query);
 
-                ResultSet resultSet = statement.executeQuery("SELECT * FROM patients");
-                String name = resultSet.getString(2);
-                String surname = resultSet.getString(3);
-                int age = resultSet.getInt(4);
-                String address = resultSet.getString(5);
-                boolean insurance = resultSet.getBoolean(6);
-                float payment = resultSet.getFloat(7);
-                System.out.println(name + " " + surname + " " + age + " " + address + " " + insurance + " " + payment);
-            }catch(Exception e){
-                System.out.println(e);
-            }
+        }catch (Exception e){
+            System.out.println(e);
         }
+    }
+    public void createTableApp(Connection conn,String table_name){
+        Statement statement;
+        try {
+            String query="create table "+table_name+"(id serial primary key, name varchar(20), " +
+                    "surname varchar(20), specialty varchar(20), time varchar(50));";
 
+            String createTableApp = "CREATE TABLE appointment AS " +
+                    "SELECT patients.iin, t1.col2, t2.col3 " +
+                    "FROM table1 t1 " +
+                    "JOIN table2 t2 ON t1.id = t2.id";
+            statement=conn.createStatement();
+            statement.executeUpdate(query);
+
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
 
 
 }
--- src/obkatka/Main.java
+++ src/obkatka/Main.java
@@ -8,11 +8,12 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         DbFunctions db = new DbFunctions();
-        Connection conn = db.connect_to_db("postgres", "postgres", "atterol");
+        Connection conn=db.connect_to_db("postgres","postgres","atterol");
 
-        db.createTable(conn, "patients");
+        db.createTable(conn,"patients");
+        db.createTableDoc(conn,"doctors");
 
-        while (true) {
+        while(true){
             System.out.println("1. List all patients");
             System.out.println("2. Update information");
             System.out.println("3. Add a patient");
@@ -22,68 +23,104 @@ public class Main {
 
             int command = scanner.nextInt();
 
-            if (command == 1) {
+            if(command == 1){
                 Statement statement = conn.createStatement();
                 String SQL_SELECT_TASKS = "select * from patients";
                 ResultSet result = statement.executeQuery(SQL_SELECT_TASKS);
 
-                while (result.next()) {
+                while(result.next()){
                     System.out.println(result.getInt("id") + " " +
-                            result.getInt("IIN") + " " +
+                            result.getFloat("IIN") + " " +
                             result.getString("name") + " " +
-                            result.getString("surname") + " " +
-                            result.getString("age") + " " +
-                            result.getString("address") + " " +
-                            result.getString("insurance") + " " +
+                            result.getString("surname")  + " " +
+                            result.getBoolean("gender")  + " " +
+                            result.getInt("age")  + " " +
+                            result.getString("address")  + " " +
+                            result.getBoolean("insurance") + " " +
                             result.getString("payment"));
                 }
-            } else if (command == 2) {
+            }
+            else if(command == 2){
                 String sql = "update patients set payment = 'Done' where id = ?";
                 PreparedStatement preparedStatement = conn.prepareStatement(sql);
                 System.out.println("Input patient id:");
                 int patientId = scanner.nextInt();
                 preparedStatement.setInt(1, patientId);
                 preparedStatement.executeUpdate();
-            } else if (command == 3) {
-                String sql = "insert into patients(IIN, name, surname, age, address, insurance, payment) values(?, ?, ?, ?, ?, ?, ?);";
+            }
+            else if(command == 3){
+                String sql = "insert into patients(IIN, name, surname, gender, age, address, insurance, payment) values(?, ?, ?, ?, ?, ?, ?, ?);";
                 PreparedStatement preparedStatement = conn.prepareStatement(sql);
                 System.out.println("Add a patient: ");
                 scanner.nextLine();
-                String IIN = scanner.nextLine();
+                System.out.println("Input patient IIN:");
+                float IIN = Float.parseFloat(scanner.nextLine());
+                System.out.println("Input patient name:");
                 String name = scanner.nextLine();
+                System.out.println("Input patient surname:");
                 String surname = scanner.nextLine();
-                String age = scanner.nextLine();
+                System.out.println("Input patient gender:");
+                boolean gender = Boolean.parseBoolean(scanner.nextLine());
+                System.out.println("Input patient age:");
+                int age = Integer.parseInt(scanner.nextLine());
+                System.out.println("Input patient address:");
                 String address = scanner.nextLine();
-                String insurance = scanner.nextLine();
+                System.out.println("Input patient insurance:");
+                boolean insurance = Boolean.parseBoolean(scanner.nextLine());
+                System.out.println("Input service fee:");
                 String payment = scanner.nextLine();
 
-                preparedStatement.setString(1, IIN);
+                preparedStatement.setFloat(1, IIN);
                 preparedStatement.setString(2, name);
                 preparedStatement.setString(3, surname);
-                preparedStatement.setString(4, age);
-                preparedStatement.setString(5, address);
-                preparedStatement.setString(6, insurance);
-                preparedStatement.setString(7, payment);
+                preparedStatement.setBoolean(4, gender);
+                preparedStatement.setInt(5, age);
+                preparedStatement.setString(6, address);
+                preparedStatement.setBoolean(7, insurance);
+                preparedStatement.setString(8, payment);
                 preparedStatement.executeUpdate();
-            } else if (command == 4) {
+            }
+
+            else if(command == 4){
                 String sql = "delete from patients where id = ?";
                 PreparedStatement preparedStatement = conn.prepareStatement(sql);
                 System.out.println("Input patient id:");
                 int patientId = scanner.nextInt();
                 preparedStatement.setInt(1, patientId);
                 preparedStatement.executeUpdate();
-            } else if (command == 6) {
-                System.exit(0);}
-
-
-              else if (command == 5) {
-                int IIN=scanner.nextInt();
-                db.search_by_UIN(conn,"patients",IIN);
             }
-              else {
+            else if(command == 5) {
+                float iinToCheck;
+                System.out.print("Enter IIN to check: ");
+                if (scanner.hasNextFloat()) {
+                    iinToCheck = scanner.nextFloat();
+                }
+                else {
+                    System.out.println("Invalid IIN entered. Exiting.");
+                    return;
+                }
+                boolean recordExists = false;
+                String query = "select iin from patients where iin = ?";
+                PreparedStatement statement = conn.prepareStatement(query);
+                statement.setFloat(1, iinToCheck);
+                ResultSet resultSet = statement.executeQuery();
+                if (resultSet.next()) {
+                    recordExists = true;
+                }
+                if (recordExists) {
+                    System.out.println("Patient with IIN " + iinToCheck + " exists in the database.");
+                }
+                else {
+                    System.out.println("Patient with IIN " + iinToCheck + " does not exist in the database.");
+                }
+            }
+            else if (command == 6){
+                System.exit(0);
+            }
+
+            else{
                 System.err.println("Command not found");
             }
         }
     }
-
 }
