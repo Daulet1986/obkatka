82ff01998fedcf96cb3255eedab4e8aa7ff511cf
Aliye <221288@astanait.edu.kz>
2023-02-17 12:38:10 +0600

    search function

--- src/obkatka/Appointment.java
+++
@@ -1,7 +0,0 @@
-package obkatka;
-
-public class Appointment {
-    Payment payment = new Payment();
-    private Float amount = payment.getPrice();
-    private Boolean paidSuccessful = payment.processPayment(amount);
-}
